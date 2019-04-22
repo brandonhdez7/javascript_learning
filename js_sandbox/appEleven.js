c9a68c08c85d2a2e869f0a1ac078a66c38ce4523
brandon <brandonhdez7@gmail.com>
2019-04-21 20:47:01 -0400

    javascript101

--- js_sandbox/appEleven.js
+++ js_sandbox/appEleven.js
@@ -26,6 +26,28 @@ console.log(square(8))
 
 // immidiatley inokable fucntion expressions - ifies
 
-(function(){
-    console.log('IIFE run ..');
-})
+// (function(name){
+//     console.log('Hello ' + name);
+// })('Brad);
+
+
+
+
+// property methods
+
+const todo = {
+    add: function(){
+        console.log("Add Todo ..")
+    },
+    edit: function(id){
+        console.log(`Edit Todo ${id}`)
+    }
+}
+
+todo.delete = function(){
+    console.log('Delete Todo ..')
+}
+
+todo.add();
+todo.edit(22)
+todo.delete()
