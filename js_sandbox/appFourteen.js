d6505cc75f55bcf0d978b76fe3628de362440604
brandon <brandonhdez7@gmail.com>
2019-04-24 23:58:45 -0400

    javascript101

---
+++ js_sandbox/appFourteen.js
@@ -0,0 +1,28 @@
+//global scope
+
+var a = 1;
+let b = 2;
+const c = 3;
+console.log('global scope: ' , a, b, c);
+
+
+function test(){
+    var a = 4;
+    let b = 5;
+    const c = 6; 
+    console.log('function scope: ' , a, b, c)
+}
+test();
+
+if(true){
+    //block scope
+    var a = 4;
+    let b = 5;
+    const c = 6; 
+    console.log('if scope: ' , a, b, c)
+}
+
+
+for(let a = 0; a < 10; a ++){
+    console.log(`loop: ${a}`)
+}
