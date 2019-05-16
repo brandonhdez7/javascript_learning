72e4bbfc982d1c9b2646bd502b0cbb72313472c3
brandon <brandonhdez7@gmail.com>
2019-05-15 22:38:18 -0400

    javascript101

--- fetch_api/app.js
+++ fetch_api/app.js
@@ -8,9 +8,31 @@ function loadData(){
     //open
     xhr.open('GET', 'data.txt', true);
 
-    xhr.onload = function(){
+    console.log('readystate', xhr.responseText)
 
+    xhr.onload = function(){
+        if(this.status === 200){
+            // console.log(this.responseText)
+            document.getElementById('output').innerHTML = 
+            `<h1>{this.responseText}</h1>`
+        }
     }
+    xhr.onreadystatechange = function(){
+        console.log('readystate', xhr.responseText)
+        if(this.status === 200 && this.readyState === 4){
+            console.log(this.responseText)
+        }
+    }
+
+    xhr.send()
+
+
+    //ready state values
+    //0: request not initialized
+    //1: server connection established
+    //2: request received
+    //3: processing request
+    //4:request finished and responce is ready
 
 
     // http statuses
